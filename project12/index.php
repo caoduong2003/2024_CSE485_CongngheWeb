3e5ea45465b19f96e01b63ac89427444dbb48343
ThanhCongnhom14 <thanhhocong28121995@gmail.com>
2024-02-23 11:37:30 +0700

    them project12

---
+++ project12/index.php
@@ -0,0 +1,32 @@
+import "index.css";
+<?php
+$navItems = [
+    "GIỚI THIỆU",
+    "TIN TỨC VÀ THÔNG BÁO",
+    "TUYỂN SINH",
+    "ĐÀO TẠO",
+    "NGHIÊN CỨU",
+    "ĐỐI NGOẠI",
+    "VĂN BẢN",
+    "SINH VIÊN",
+    "LIÊN HỆ"
+];
+echo '<nav><ul>';
+foreach ($navTtems as $item){
+    echo "<li>$item</li>";
+}
+echo '</ul></nav>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
