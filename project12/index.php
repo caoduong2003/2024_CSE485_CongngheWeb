38df54ae44bc1e701a925c0c196aba8f79c785ed
ThanhCongnhom14 <thanhhocong28121995@gmail.com>
2024-02-23 16:35:35 +0700

    hoan thien project12

--- project12/index.php
+++ project12/index.php
@@ -1,8 +1,7 @@
-import "index.css";
 <?php
-$navItems = [
+ $navItems = [
     "GIỚI THIỆU",
-    "TIN TỨC VÀ THÔNG BÁO",
+    "TIN TỨC & THÔNG BÁO",
     "TUYỂN SINH",
     "ĐÀO TẠO",
     "NGHIÊN CỨU",
@@ -11,11 +10,15 @@ $navItems = [
     "SINH VIÊN",
     "LIÊN HỆ"
 ];
-echo '<nav><ul>';
-foreach ($navTtems as $item){
+echo '<nav class="nav"><ul>';
+foreach ($navItems as $item)
+{
+    
     echo "<li>$item</li>";
+    
 }
 echo '</ul></nav>';
+
 ?>
 
 <!DOCTYPE html>
@@ -23,10 +26,13 @@ echo '</ul></nav>';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css"> 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+
     <title>Document</title>
 </head>
 <body>
-    
+<i class="fa-solid fa-house" style="color: white;" ></i>
+
 </body>
 </html>
