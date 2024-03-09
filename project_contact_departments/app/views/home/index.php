d9e45b5b153d77a1af42b1ebf96913eca44e6fa6
Dương Vũ <caoduong2742@gmail.com>
2024-03-10 05:38:44 +0700

    PageHome

--- project_contact_departments/app/views/home/index.php
+++ project_contact_departments/app/views/home/index.php
@@ -1,1 +1,85 @@
-// hiển thị các khoa và giới thiệu
+<?php
+// hiển thị các khoa và giới thiệu'
+include("../../views/navbar/index.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../../public/assets/css/styleHome.css">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <div class="row row-cols-1 row-cols-md-3 g-3 py-3 px-3 m-0">
+        <!-- động -->
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="card text-center">
+                    <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
+                        alt="image" />
+                    <div class="card-body">
+                        <h5>
+                            <p class="card-title">Khoa CNTT </p>
+                        </h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="card text-center">
+                    <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
+                        alt="image" />
+                    <div class="card-body">
+                        <h5>
+                            <p class="card-title">Khoa CNTT </p>
+                        </h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="card text-center">
+                    <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
+                        alt="image" />
+                    <div class="card-body">
+                        <h5>
+                            <p class="card-title">Khoa CNTT </p>
+                        </h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="card text-center">
+                    <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
+                        alt="image" />
+                    <div class="card-body">
+                        <h5>
+                            <p class="card-title">Khoa CNTT </p>
+                        </h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <footer>
+        <?php
+        include("../../views/footer/index.php");
+        ?>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
---
+++ project_contact_departments/app/views/navbar/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-light bg-body-tertiary">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">Danh bạ điện tử</a>
+            <form class="d-flex input-group w-75">
+                <input type="search" class="form-control rounded" placeholder="Tìm kiếm" aria-label="Search"
+                    aria-describedby="search-addon" />
+                <span class="input-group-text border-0" id="search-addon">
+                    <i class="fas fa-search"></i>
+                </span>
+            </form>
+            <a href="../users/index.php" class="navbar-text text-decoration-none">
+                <span class="ms-2">tên của bố ở đây</span>
+                <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" width="30" height="30"
+                    class="rounded-circle" />
+            </a>
+
+
+
+        </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
