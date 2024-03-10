49fa8e8ad82b6a2d83a0ce57f376775d70091980
Dương Vũ <caoduong2742@gmail.com>
2024-03-10 16:57:40 +0700

    changeHome

--- project_contact_departments/app/views/employees/index.php
+++ project_contact_departments/app/views/employees/index.php
@@ -94,41 +94,44 @@ $totalPages = ceil(count($products) / $itemsPerPage);
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../../../public/assets/css/style.css">
-    <title>Document</title>
+    <title>Employees</title>
 </head>
+
 <body>
-<div class="product-list">
-    <?php foreach ($currentPageItems as $product): ?>
+    <div class="product-list">
+        <?php foreach ($currentPageItems as $product): ?>
         <div class="product">
             <a href="detail.php?id=<?php echo $product['id']; ?>">
-                <img class="pic" src="<?php echo $product['image']; ?>" >
+                <img class="pic" src="<?php echo $product['image']; ?>">
                 <p>Tên Nhân Viên: <?php echo $product['name']; ?></p>
                 <p>SDT: <?php echo $product['SDT']; ?></p>
                 <p><?php echo $product['description']; ?></p>
             </a>
         </div>
-    <?php endforeach; ?>
-</div>
-<div class="pagination">
-    <?php if ($currentPage > 1): ?>
+        <?php endforeach; ?>
+    </div>
+    <div class="pagination">
+        <?php if ($currentPage > 1): ?>
         <a href="?page=<?php echo $currentPage - 1; ?>">Previous</a>
-    <?php endif; ?>
-    <?php for ($i = 1; $i <= $totalPages; $i++): ?>
-        <?php if ($i == $currentPage): ?>
-            <span class="active"><?php echo $i; ?></span>
-        <?php else: ?>
-            <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
         <?php endif; ?>
-    <?php endfor; ?>
-    <?php if ($currentPage < $totalPages): ?>
+        <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+        <?php if ($i == $currentPage): ?>
+        <span class="active"><?php echo $i; ?></span>
+        <?php else: ?>
+        <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+        <?php endif; ?>
+        <?php endfor; ?>
+        <?php if ($currentPage < $totalPages): ?>
         <a href="?page=<?php echo $currentPage + 1; ?>">Next</a>
-    <?php endif; ?>
-</div>
+        <?php endif; ?>
+    </div>
 </body>
+
 </html>
--- project_contact_departments/app/views/home/index.php
+++ project_contact_departments/app/views/home/index.php
@@ -6,8 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../../public/assets/css/styleHome.css">
+    <link rel="stylesheet" href='../../../public/assets/css/styleHome.css'>
     <title>Home Page</title>
+    <!-- <style>
+    .card {
+        background-color: #f4f4f4;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+    }
+
+    .card:hover {
+        background-color: #d941ff15;
+        transition: 0.5s;
+
+    }
+
+
+
+    .card>img {
+        width: 100px;
+        height: 100px;
+        margin: 0 auto;
+        display: block;
+        object-fit: cover;
+    }
+    </style> -->
+
 </head>
 
 <body>
@@ -19,12 +44,11 @@
     // require_once ROOT.'app/controllers/DepartmentController.php';
     ?>
 
-    <div class="row row-cols-1 row-cols-md-4 g-3 py-3 px-3 m-0">
-        <!-- động -->
+    <div class="row row-cols-1 row-cols-md-4 g-3 py-3 px-3 m-0 ">
         <?php foreach ($departments as $department): ?>
         <div class="col">
             <a href="#" class="text-decoration-none">
-                <div class="card text-center">
+                <div class="card text-center" id=cardid>
                     <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
                         alt="image" />
                     <div class="card-body">
