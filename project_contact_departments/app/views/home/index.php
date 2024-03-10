5d848688487d4a83dec2920a9bb59b5879ddfd75
Dương Vũ <caoduong2742@gmail.com>
2024-03-10 17:15:33 +0700

    changHome2

--- project_contact_departments/app/views/home/index.php
+++ project_contact_departments/app/views/home/index.php
@@ -3,35 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href='../../../public/assets/css/styleHome.css'>
+    <link rel="stylesheet" href='../public/assets/css/styleHome.css'>
     <title>Home Page</title>
-    <!-- <style>
-    .card {
-        background-color: #f4f4f4;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 
-    }
-
-    .card:hover {
-        background-color: #d941ff15;
-        transition: 0.5s;
-
-    }
-
-
-
-    .card>img {
-        width: 100px;
-        height: 100px;
-        margin: 0 auto;
-        display: block;
-        object-fit: cover;
-    }
-    </style> -->
 
 </head>
 
@@ -43,30 +21,32 @@
 
     // require_once ROOT.'app/controllers/DepartmentController.php';
     ?>
+    <div class="container">
 
-    <div class="row row-cols-1 row-cols-md-4 g-3 py-3 px-3 m-0 ">
-        <?php foreach ($departments as $department): ?>
-        <div class="col">
-            <a href="#" class="text-decoration-none">
-                <div class="card text-center" id=cardid>
-                    <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
-                        alt="image" />
-                    <div class="card-body">
-                        <h5>
-                            <p class="card-title">
-                                <?= $department->getDepartmentName() ?>
-                            </p>
-                            <p class="card-text">
-                                <?= $department->getEmail() ?>
-                            </p>
-                        </h5>
-                    </div>
+        <div class="row row-cols-1 row-cols-md-4 g-3 py-3 px-3 m-0 ">
+            <?php foreach ($departments as $department): ?>
+                <div class="col">
+                    <a href="#" class="text-decoration-none">
+                        <div class="card text-center">
+                            <img src="https://adtimin.vn/wp-content/uploads/2017/09/Logo-1.jpg" class="card-img-top"
+                                alt="image" />
+                            <div class="card-body">
+                                <h5>
+                                    <p class="card-title">
+                                        <?= $department->getDepartmentName() ?>
+                                    </p>
+                                    <p class="card-text">
+                                        <?= $department->getEmail() ?>
+                                    </p>
+                                </h5>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-            </a>
+
+            <?php endforeach; ?>
+
         </div>
-
-        <?php endforeach; ?>
-
     </div>
     <footer>
         <?php
@@ -79,7 +59,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+        </script>
 </body>
 
 </html>
