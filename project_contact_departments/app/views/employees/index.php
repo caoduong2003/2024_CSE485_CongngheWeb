6bd69f4727b0e92e7a9a82190b9e3a8622469fe0
Nguyen Hoang Hiep <hiep09012003@gmail.com>
2024-03-10 15:03:53 +0700

    update

--- project_contact_departments/app/views/employees/index.php
+++ project_contact_departments/app/views/employees/index.php
@@ -99,7 +99,7 @@ $totalPages = ceil(count($products) / $itemsPerPage);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="asscets/style.css">
+    <link rel="stylesheet" href="../../../public/assets/css/style.css">
     <title>Document</title>
 </head>
 <body>
