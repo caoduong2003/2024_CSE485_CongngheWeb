f3eae0e8a231d8856fdd74854a7ef216740dd1d5
Nguyen Hoang Hiep <hiep09012003@gmail.com>
2024-03-11 17:13:49 +0700

    doi

--- project_contact_departments/public/index.php
+++ project_contact_departments/public/index.php
@@ -9,22 +9,23 @@ require_once ROOT . '/app/controllers/UserController.php';
 
 // require_once ROOT.'/app/services/DepartmentService.php';
 
-// $adminController = new EmployeeController();
-// $adminController->index();
+$adminController = new EmployeeController();
+$adminController->index();
 
 // $userController = new UserController();
 
 // $userController->login();
 
-$url = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
-$i = 1;
+// $url = $_SERVER['REQUEST_URI'];
+// $method = $_SERVER['REQUEST_METHOD'];
+// $data = $_POST;
 
-if($url ==='/' && $method === 'GET'){
-  $departmentController = new DepartmentController();
-  $departmentController->getAllDepartment();  
-}
-echo $method;
-echo '<br>';
-echo $url;
-
+// if($url ==='/' && $method === 'GET'){
+//   $departmentController = new DepartmentController();
+//   $departmentController->getAllDepartment();  
+// }
+// echo $method;
+// echo '<br>';
+// echo $url;
+// echo '<br>';
+// echo $data;
